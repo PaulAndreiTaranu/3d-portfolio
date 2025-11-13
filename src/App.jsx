8af7bb572a814ea6215b6ec037dd7ff5ad8e88b9
Paul Andrei Taranu <paulandreitaranu@gmail.com>
2025-11-13 17:18:22 +0000

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
-    const [count, setCount] = useState(0)
-
-    return <div className=''>HOLA</div>
+    return (
+        <>
+            <Navbar />
+        </>
+    )
 }
 
 export default App
